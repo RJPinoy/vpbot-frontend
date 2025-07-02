f7cecd0cec72c8a1db6012dc813fc27441632884
Rejen <rejenkiller@live.fr>
2025-07-02 17:16:22 +0200

    :new: Moved routes for better structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import './fontawesome/library.js'
 import { ModalProvider } from './components/modals/ModalProvider.jsx'
 import ModalHandler from './components/modals/modalHandler.jsx'
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { RouterProvider } from "react-router";
 import { Provider } from 'react-redux'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    index: true,
-    Component: App,
-  },
-]);
+import store from './stores/store.js'
+import { router } from './routes/routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
---
+++ src/routes/routes.jsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter, Navigate } from 'react-router'
+import App from '../App.jsx'
+import Login from '../components/pages/Login.jsx'
+import ProtectedRoute from './ProtectedRoute.jsx'
+import NavLayout from '../components/layouts/NavLayout.jsx'
+import Support from '../components/pages/Support.jsx'
+import Tools from '../components/pages/Tools.jsx'
+import Documentation from '../components/pages/Documentation.jsx'
+import AdminDashboard from '../components/pages/admin/AdminDashboard.jsx'
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Navigate to="/login" replace />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+    {
+        path: '/',
+        element: <ProtectedRoute />,
+        children: [
+        {
+            element: <NavLayout />,
+            children: [
+                { path: 'dashboard', element: <App /> },
+                { path: 'support', element: <Support /> },
+                { path: 'tools', element: <Tools /> },
+                { path: 'documentation', element: <Documentation /> },
+            ],
+        },
+        ],
+    },
+    {
+        path: '/admin/',
+        element: <ProtectedRoute adminOnly={true} />,
+        children: [
+        {
+            element: <NavLayout />,
+            children: [
+                { path: 'dashboard', element: <AdminDashboard /> },
+            ]
+        },
+        ],
+    },
+]);
