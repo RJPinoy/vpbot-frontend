95776cac2a1450d02966e441dc730c5637b2e080
RJPinoy <rejenaligoradarsonval@gmail.com>
2025-07-15 09:07:20 +0200

    :update: added icon

--- src/fontawesome/library.js
+++ src/fontawesome/library.js
@@ -1,18 +1,15 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faXmark, faEye, faEyeSlash, faUser, faCog, faHouse, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faXmark, faEye, faEyeSlash, faUser, faCog, faHouse, faMagnifyingGlass, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 library.add(
     // Eye icons for password visibility toggle
     faEye, faEyeSlash,
 
-    // Cancel icon for modal close or cancel actions
-    faXmark,
-
     // User icon for user-related actions or profiles
     faUser, faHouse,
 
     // Settings icon for configuration or settings pages
-    faCog,
+    faCog, faXmark, faCheck,
 
     faMagnifyingGlass,
 );
