471d0040b353200615d959628db2fd631f097ad1
RJPinoy <rejenaligoradarsonval@gmail.com>
2025-08-06 15:36:12 +0200

    :fix: Fix name

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './fontawesome/library.js'
 import { ModalProvider } from './components/modals/ModalProvider.jsx'
-import ModalHandler from './components/modals/modalHandler.jsx'
+import ModalHandler from './components/modals/ModalHandler.jsx'
 import { RouterProvider } from "react-router";
 import { Provider } from 'react-redux'
 import store from './stores/store.js'
